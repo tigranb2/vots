de50c26a7b91bb6273377d91aacf4073d427cf38
Tigran Bantikyan <59093401+tigranb2@users.noreply.github.com>
2025-05-03 10:30:22 -0700

    feat: red black tree insert implementation (#3)
    * feat: red black tree header
    
    * build: updated linters to fix saving issue
    
    * feat: red black tree insertion logic
    
    * fix: insert test setup
    
    * test: red black tree insert testing

---
+++ src/include/storage/red_black_tree.h
@@ -0,0 +1,54 @@
+#pragma once
+
+namespace vots {
+
+#define RBTREE_TEMPLATE template <typename KeyType, typename DataType>
+#define RBTREE_TYPE RedBlackTree<KeyType, DataType>
+
+/*
+ * RedBlackTree implements a Red-Black tree, and is used to store limit objects.
+ *
+ * The self-balancing property of Red-Black trees ensures that the bid and ask limit trees always provide worst-case
+ * O(log m) operations, where m is the size of the tree. A Red-Black tree is preferred over other self-balancing
+ * trees as its relaxed balancing property result in fewer (costly) rotations in the write-heavy workloads typical
+ * of the market. This comes at the trade-off of less-efficient lookups, but the use of a hashmap for tracking limit
+ * prices eliminates the need for lookups in the limit trees altogether.
+ */
+RBTREE_TEMPLATE class RedBlackTree {
+   public:
+    RedBlackTree();
+
+    // Find returns a nullptr if the data with the specified key was not found
+    auto Find(KeyType key) -> DataType *;
+    void Insert(KeyType key, DataType data);
+    void Delete(KeyType key);
+
+    auto ValidateTree() -> bool;
+
+   private:
+    struct Node {
+        DataType data_;
+        Node *parent_;
+        Node *left_;
+        Node *right_;
+        KeyType key_;
+        bool is_red_;
+    };
+
+    // InsertFix restores violated tree invariants, if any are present, after an insert
+    void InsertFix(Node *node);
+    // DeleteFix restores violated tree invariants, if any are present, after a delete
+    void DeleteFix(Node *node);
+
+    void RotateLeft(Node *node);
+    void RotateRight(Node *node);
+
+    // NewNode returns a red node with the specified key, data, and parent
+    inline auto NewNode(KeyType key, DataType data, Node *parent) -> Node * {
+        return new Node{data, parent, nullptr, nullptr, key, true};
+    }
+
+    Node *root_;
+};
+
+}  // namespace vots
---
+++ src/storage/red_black_tree.cpp
@@ -0,0 +1,201 @@
+#include "storage/red_black_tree.h"
+
+#include <functional>
+#include <string>
+
+namespace vots {
+
+RBTREE_TEMPLATE
+RBTREE_TYPE::RedBlackTree() : root_(nullptr){};
+
+RBTREE_TEMPLATE
+auto RBTREE_TYPE::Find(KeyType key) -> DataType * {
+    Node *cur = this->root_;
+    while (cur != nullptr) {
+        if (cur->key_ == key) {
+            return &cur->data_;
+        }
+        if (key < cur->key_) {
+            cur = cur->left_;
+        } else {
+            cur = cur->right_;
+        }
+    }
+    return nullptr;
+}
+
+RBTREE_TEMPLATE
+void RBTREE_TYPE::Insert(KeyType key, DataType data) {
+    if (this->root_ == nullptr) {
+        this->root_ = this->NewNode(key, data, nullptr);
+        this->root_->is_red_ = false;
+        return;
+    }
+
+    Node *prev = nullptr;
+    Node *cur = this->root_;
+    while (cur != nullptr) {
+        prev = cur;
+
+        if (key < cur->key_) {
+            cur = cur->left_;
+        } else {
+            cur = cur->right_;
+        }
+    }
+
+    Node *new_node = this->NewNode(key, data, prev);
+    if (key < prev->key_) {
+        prev->left_ = new_node;
+    } else {
+        prev->right_ = new_node;
+    }
+    this->InsertFix(new_node);
+}
+
+RBTREE_TEMPLATE
+auto RBTREE_TYPE::ValidateTree() -> bool {
+    using Node = typename RBTREE_TYPE::Node;
+
+    std::function<int(const Node *, bool &is_red)> validate_children = [&](const Node *node, bool &is_red) -> int {
+        if (node == nullptr) {
+            is_red = false;
+            return 1;
+        }
+
+        is_red = node->is_red_;
+        bool left_is_red = false;
+        bool right_is_red = false;
+        int left_height = validate_children(node->left_, left_is_red);
+        int right_height = validate_children(node->right_, right_is_red);
+
+        if ((left_height != right_height) || (is_red && (left_is_red || right_is_red))) {
+            return -1;
+        }
+        return left_height + (is_red ? 0 : 1);
+    };
+
+    bool is_red;
+    return validate_children(this->root_, is_red);
+}
+
+RBTREE_TEMPLATE
+void RBTREE_TYPE::InsertFix(Node *node) {
+    // Red node's parent cannot be red
+    while (node->parent_ != nullptr && node->parent_->is_red_) {
+        // Cannot be nullptr since node->parent_'s red & root must be black
+        Node *grandparent = node->parent_->parent_;
+        Node *uncle;
+        if (node->parent_ == grandparent->left_) {
+            uncle = grandparent->right_;
+
+            // Red uncle --> recolor grandparent and its children
+            if (uncle != nullptr && uncle->is_red_) {
+                uncle->is_red_ = false;
+                node->parent_->is_red_ = false;
+                grandparent->is_red_ = true;
+                node = grandparent;
+                continue;
+            }
+
+            // Node *just* before grandparent in sorted order
+            if (node->parent_->right_ == node) {
+                node = node->parent_;
+                this->RotateLeft(node);
+                grandparent = node->parent_->parent_;
+            }
+
+            // Node is left child of grandparent's left child
+            node->parent_->is_red_ = false;
+            grandparent->is_red_ = true;
+            RotateRight(grandparent);
+        } else {
+            uncle = grandparent->left_;
+
+            // Red uncle --> recolor grandparent and its children
+            if (uncle != nullptr && uncle->is_red_) {
+                uncle->is_red_ = false;
+                node->parent_->is_red_ = false;
+                grandparent->is_red_ = true;
+                node = grandparent;
+                continue;
+            }
+
+            // Node *just* after grandparent in sorted order
+            if (node->parent_->left_ == node) {
+                node = node->parent_;
+                this->RotateRight(node);
+                grandparent = node->parent_->parent_;
+            }
+
+            // Node is right child of grandparent's right child
+            node->parent_->is_red_ = false;
+            grandparent->is_red_ = true;
+            RotateLeft(grandparent);
+        }
+    }
+    this->root_->is_red_ = false;
+}
+
+RBTREE_TEMPLATE
+void RBTREE_TYPE::RotateLeft(Node *node) {
+    // Given that the method is private, assume that neither node nor node.right
+    // are nullptr
+    Node *new_parent = node->right_;
+
+    // Update parent's data
+    if (node == this->root_) {
+        this->root_ = new_parent;
+    } else {
+        if (node == node->parent_->left_) {
+            node->parent_->left_ = new_parent;
+        } else {
+            node->parent_->right_ = new_parent;
+        }
+    }
+
+    // Update parentage
+    new_parent->parent_ = node->parent_;
+    node->parent_ = new_parent;
+    if (new_parent->left_) {
+        new_parent->left_->parent_ = node;
+    }
+
+    // Update children
+    node->right_ = new_parent->left_;
+    new_parent->left_ = node;
+}
+
+RBTREE_TEMPLATE
+void RBTREE_TYPE::RotateRight(Node *node) {
+    // Given that the method is private, assume that neither node nor node.left
+    // are nullptr
+    Node *new_parent = node->left_;
+
+    // Update parent's data
+    if (node == this->root_) {
+        this->root_ = new_parent;
+    } else {
+        if (node == node->parent_->left_) {
+            node->parent_->left_ = new_parent;
+        } else {
+            node->parent_->right_ = new_parent;
+        }
+    }
+
+    // Updae parentage
+    new_parent->parent_ = node->parent_;
+    node->parent_ = new_parent;
+    if (new_parent->right_) {
+        new_parent->right_->parent_ = node;
+    }
+
+    // Update children
+    node->left_ = new_parent->right_;
+    new_parent->right_ = node;
+}
+
+template class RedBlackTree<int, std::string>;
+template class RedBlackTree<int, int>;
+
+}  // namespace vots

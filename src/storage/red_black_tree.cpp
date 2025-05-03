dbaabd714bdd79822fba9de742a97480ceaa506e
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-05-03 10:29:59 -0700

    test: red black tree insert testing

--- src/storage/red_black_tree.cpp
+++ src/storage/red_black_tree.cpp
@@ -1,5 +1,8 @@
 #include "storage/red_black_tree.h"
 
+#include <functional>
+#include <string>
+
 namespace vots {
 
 RBTREE_TEMPLATE
@@ -12,7 +15,7 @@ auto RBTREE_TYPE::Find(KeyType key) -> DataType * {
         if (cur->key_ == key) {
             return &cur->data_;
         }
-        if (cur->key_ < key) {
+        if (key < cur->key_) {
             cur = cur->left_;
         } else {
             cur = cur->right_;
@@ -59,17 +62,17 @@ auto RBTREE_TYPE::ValidateTree() -> bool {
             is_red = false;
             return 1;
         }
-        is_red = node->is_red_;
 
+        is_red = node->is_red_;
         bool left_is_red = false;
         bool right_is_red = false;
         int left_height = validate_children(node->left_, left_is_red);
         int right_height = validate_children(node->right_, right_is_red);
 
-        if (is_red && (left_is_red || right_is_red)) {
+        if ((left_height != right_height) || (is_red && (left_is_red || right_is_red))) {
             return -1;
         }
-        return left_height == right_height ? left_height + 1 : -1;
+        return left_height + (is_red ? 0 : 1);
     };
 
     bool is_red;
@@ -87,7 +90,7 @@ void RBTREE_TYPE::InsertFix(Node *node) {
             uncle = grandparent->right_;
 
             // Red uncle --> recolor grandparent and its children
-            if (uncle->is_red_) {
+            if (uncle != nullptr && uncle->is_red_) {
                 uncle->is_red_ = false;
                 node->parent_->is_red_ = false;
                 grandparent->is_red_ = true;
@@ -98,9 +101,8 @@ void RBTREE_TYPE::InsertFix(Node *node) {
             // Node *just* before grandparent in sorted order
             if (node->parent_->right_ == node) {
                 node = node->parent_;
-                grandparent = node->parent_->parent_;
                 this->RotateLeft(node);
-                // Continue to case below
+                grandparent = node->parent_->parent_;
             }
 
             // Node is left child of grandparent's left child
@@ -111,7 +113,7 @@ void RBTREE_TYPE::InsertFix(Node *node) {
             uncle = grandparent->left_;
 
             // Red uncle --> recolor grandparent and its children
-            if (uncle->is_red_) {
+            if (uncle != nullptr && uncle->is_red_) {
                 uncle->is_red_ = false;
                 node->parent_->is_red_ = false;
                 grandparent->is_red_ = true;
@@ -122,8 +124,8 @@ void RBTREE_TYPE::InsertFix(Node *node) {
             // Node *just* after grandparent in sorted order
             if (node->parent_->left_ == node) {
                 node = node->parent_;
-                grandparent = node->parent_->parent_;
                 this->RotateRight(node);
+                grandparent = node->parent_->parent_;
             }
 
             // Node is right child of grandparent's right child
@@ -141,9 +143,15 @@ void RBTREE_TYPE::RotateLeft(Node *node) {
     // are nullptr
     Node *new_parent = node->right_;
 
-    // Update root
+    // Update parent's data
     if (node == this->root_) {
         this->root_ = new_parent;
+    } else {
+        if (node == node->parent_->left_) {
+            node->parent_->left_ = new_parent;
+        } else {
+            node->parent_->right_ = new_parent;
+        }
     }
 
     // Update parentage
@@ -164,9 +172,15 @@ void RBTREE_TYPE::RotateRight(Node *node) {
     // are nullptr
     Node *new_parent = node->left_;
 
-    // Update root
+    // Update parent's data
     if (node == this->root_) {
         this->root_ = new_parent;
+    } else {
+        if (node == node->parent_->left_) {
+            node->parent_->left_ = new_parent;
+        } else {
+            node->parent_->right_ = new_parent;
+        }
     }
 
     // Updae parentage
@@ -182,5 +196,6 @@ void RBTREE_TYPE::RotateRight(Node *node) {
 }
 
 template class RedBlackTree<int, std::string>;
+template class RedBlackTree<int, int>;
 
 }  // namespace vots

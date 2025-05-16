c75dd0b9223a0fe089d774c26a26be817059b16e
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-05-15 23:19:31 -0700

    refactor: public pointers

--- src/include/storage/list_node.h
+++ src/include/storage/list_node.h
@@ -17,25 +17,26 @@ LN_TEMPLATE class ListNode {
    public:
     KeyType key_;
     DataType data_;
+    std::unique_ptr<ListNode> next_;
+    ListNode *prev_;
 
     // Make constructs a ListNode object, wrapped in an std::unique_ptr
     static auto Make(KeyType key, DataType data) -> std::unique_ptr<ListNode>;
 
     // Add adds the new node to the end of the provide prev node. It returns a reference to the new node.
-    auto Add(std::unique_ptr<ListNode> &prev, std::unique_ptr<ListNode> &new_node) -> std::unique_ptr<ListNode> &;
+    //
+    // Neither the provided prev nor the new_node can have an underlying nullptr.
+    auto Add(std::unique_ptr<ListNode> &prev, std::unique_ptr<ListNode> new_node) -> std::unique_ptr<ListNode> &;
 
     auto Add(std::unique_ptr<ListNode> &prev, KeyType key, DataType data) -> std::unique_ptr<ListNode> &;
 
     void Delete(std::unique_ptr<ListNode> &to_delete);
 
     // GetOwner returns a pointer to the unique_ptr owning the provided ListNode pointer. If the head node is provided
-    // the owner cannot be determined and a nullptr is returned instead.
+    // the owner is not part of the list and thus cannot be determined, so a nullptr is returned instead.
     auto GetOwner(ListNode *node) -> std::unique_ptr<ListNode> *;
 
    private:
-    std::unique_ptr<ListNode> next_;
-    ListNode *prev_;
-
     ListNode(KeyType key, DataType data) : key_(key), data_(std::move(data)) {};
 };
 }  // namespace vots

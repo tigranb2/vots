7f77d2b4302cc8050320354fc8817118280ce6d3
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-05-26 18:48:02 -0700

    fix: styling

--- src/include/orderbook/limit.h
+++ src/include/orderbook/limit.h
@@ -13,12 +13,8 @@ class Limit {
     int size_;
     int total_volume_;
 
-    auto GetHeadOrder() -> std::unique_ptr<ListNode<int, Order>> & {
-        return *head_order_;
-    }
-    auto GetTailOrder() -> std::unique_ptr<ListNode<int, Order>> & {
-        return *tail_order_;
-    }
+    auto GetHeadOrder() -> std::unique_ptr<ListNode<int, Order>> & { return *head_order_; }
+    auto GetTailOrder() -> std::unique_ptr<ListNode<int, Order>> & { return *tail_order_; }
 
    private:
     std::unique_ptr<ListNode<int, Order>> *head_order_;

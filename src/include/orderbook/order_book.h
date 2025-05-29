3c9b98e4551a18980224a9349648caef1a07e230
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-05-28 21:21:14 -0700

    feat: OrderBook class definition update

--- src/include/orderbook/order_book.h
+++ src/include/orderbook/order_book.h
@@ -5,18 +5,21 @@
 
 #include "limit.h"
 #include "order.h"
+#include "storage/red_black_tree.h"
 
 namespace vots {
 
-struct OrderBook {
+class OrderBook {
    public:
-    void Add(bool is_buy, int shares);
-    void Cancel();
-    void Execute();
+    OrderBook() : buy_limits_(new RedBlackTree<int, Limit>()), sell_limits_(new RedBlackTree<int, Limit>()) {};
+
+    auto Add(bool is_buy, int shares) -> int;
+    void Cancel(int order_id);
+    void Execute(int order_id);
 
    private:
-    std::unique_ptr<Limit> buy_limits_;
-    std::unique_ptr<Limit> sell_limits_;
+    std::unique_ptr<RedBlackTree<int, Limit>> buy_limits_;
+    std::unique_ptr<RedBlackTree<int, Limit>> sell_limits_;
 
     boost::unordered_flat_map<int, Limit> limit_map_;  // key: limit price
     boost::unordered_flat_map<int, Order> order_map_;  // key: order ID

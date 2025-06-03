7e3ba989cff29ea43235a3ca830ffa50cf4e1ce5
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-06-02 18:25:56 -0700

    feat: updated list node to entire linked list

---
+++ src/include/storage/linked_list.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <boost/unordered/unordered_flat_map.hpp>
+#include <memory>
+#include <vector>
+
+namespace vots {
+
+#define LL_TEMPLATE template <typename KeyType, typename DataType>
+#define LL_TYPE LinkedList<KeyType, DataType>
+
+/*
+ * LinkedList implements a simple doubly linked-list and is used to store order objects.
+ *
+ * Each node contains an identifying key, though with no uniqueness guarantee, and data. The list is supported by a
+ * hash-map for efficient look up and deletion on key
+ */
+LL_TEMPLATE class LinkedList {
+   public:
+    LinkedList();
+
+    // Add adds the new node to the end of the tail node and reassigns the tail node
+    void Add(KeyType key, DataType data);
+
+    // Remove removes the LinkedList from the list, consequently deleting it. 
+    void Remove(KeyType key);
+
+    // ValidateList checks whether the list elements are in the order specified by the expected vector
+    static auto ValidateList(const LinkedList& list, const std::vector<std::pair<KeyType, DataType>> &expected) -> bool;
+
+    auto GetHead() -> std::pair<KeyType, DataType> {
+        return {this->head_->key_, this->head_->data_};
+    }
+
+    auto GetTail() -> std::pair<KeyType, DataType> {
+        return {this->tail_->key_, this->tail_->data_};
+    }
+
+   private:
+    struct Node {
+        KeyType key_;
+        DataType data_;
+        std::unique_ptr<Node> next_;
+        Node *prev_;
+
+        Node(KeyType key, DataType data) : key_(key), data_(std::move(data)), next_(nullptr), prev_(nullptr) {};
+    };
+
+    std::unique_ptr<Node> head_;
+    Node *tail_;
+    boost::unordered_flat_map<KeyType, Node &> node_map_;
+};
+}  // namespace vots
---
+++ src/storage/linked_list.cpp
@@ -0,0 +1,50 @@
+#include "storage/linked_list.h"
+
+namespace vots {
+
+LL_TEMPLATE
+void LL_TYPE::Add(KeyType key, DataType data) {
+    auto new_node = std::unique_ptr<LinkedList::Node>(new LinkedList::Node(key, data));
+    if (this->tail_ == nullptr) {
+        head_ = std::move(new_node);
+        tail_ = this->head_.get();
+        return;
+    }
+
+    this->tail_->next_ = std::move(new_node);
+    this->tail_ = this->tail_->next_.get();
+}
+
+LL_TEMPLATE
+void LL_TYPE::Remove(KeyType key) {
+    Node &to_delete = this->node_map_.at(key);
+    if (to_delete.next_.get() != nullptr) {
+        to_delete.next_->prev_ = to_delete.prev_;
+    }
+
+    if (to_delete.prev_) {
+        to_delete.prev_->next_ = std::move(to_delete.next_);
+    }
+}
+
+LL_TEMPLATE
+auto LL_TYPE::ValidateList(const LinkedList &list, const std::vector<std::pair<KeyType, DataType>> &expected) -> bool {
+    Node *node = list.head_.get();
+    for (const auto &expect : expected) {
+        if (node == nullptr) {
+            return false;
+        }
+
+        if (node->key_ != expect.first || node->data_ != expect.second) {
+            return false;
+        }
+
+        node = node->next_.get();
+    }
+
+    return node == nullptr;
+}
+
+template class LinkedList<int, int>;
+
+}  // namespace vots
